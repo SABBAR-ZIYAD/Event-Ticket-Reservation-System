277251596baae7149bb1894fc7dc36420e2a447f
Ziad Ghalbane <98329069+zedbooy@users.noreply.github.com>
2024-12-22 19:11:31 +0100

    Update User.cpp
    by ziad

--- User.cpp
+++ User.cpp
@@ -8,12 +8,8 @@ using namespace std;
 
 User::User() : id(""), fullName(""), email(""), phone(""), eventID("") {}
 
-User::User(string Uid, string Fname, string email, string fon, string Evid)
-    : id(Uid), fullName(Fname), email(email), phone(fon), eventID(Evid) {}
-
-User User::getUserinfo() {
-    return User(id, fullName, email, phone, eventID);
-}
+User::User(string Uid, string Fname, string Email, string Phone, string Evid)
+    : id(Uid), fullName(Fname), email(Email), phone(Phone), eventID(Evid) {}
 
 string User::getId() const { 
     return id; 
@@ -51,16 +47,15 @@ void User::setPhone(const string& Phone) {
     if (Phone.size() == 10 && all_of(Phone.begin(), Phone.end(), ::isdigit)) {
         phone = Phone;
     } else {
-        cout << "Invalid phone number!" << endl;
+        cout << "Invalid phone number!\n";
     }
 }
 
 void User::inputDetails() {
-    cin.ignore(numeric_limits<streamsize>::max(), '\n'); 
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
 
     cout << "Enter Full Name: ";
     getline(cin, fullName);
-    setFullName(fullName);
 
     cout << "Enter Email: ";
     string tempEmail;
@@ -74,6 +69,6 @@ void User::inputDetails() {
 }
 
 void User::saveToFile(int eventID) {
-    cout << "Saving user details to file... \n";
+    // TODO: Implement file saving logic here.
+    cout << "Saving user details to file for event ID: " << eventID << "\n";
 }
-
